4b5a1503b669ada50246c5f49fe80d2a0ec012bb
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2025-11-28 01:02:41 -0300

    feat: add InstrumentationViewInitializer

---
+++ src/main/java/com/flowingcode/vaadin/jsonmigration/InstrumentedRoute.java
@@ -0,0 +1,46 @@
+/*-
+ * #%L
+ * Json Migration Helper
+ * %%
+ * Copyright (C) 2025 Flowing Code
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.flowingcode.vaadin.jsonmigration;
+
+import com.vaadin.flow.component.Component;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to mark a {@link Component} class for instrumented route registration.
+ *
+ * @author Javier Godoy / Flowing Code
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface InstrumentedRoute {
+
+  /**
+   * The route path for this component.
+   *
+   * @return the route path
+   */
+  String value();
+
+}
